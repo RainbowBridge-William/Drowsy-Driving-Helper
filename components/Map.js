56d7d7ea40a698d70bf34fbd1cfdaceaabcfb131
William Wang <swang45@uw.edu>
2021-12-04 13:05:40 -0800

    Added location permission request

--- components/Map.js
+++ components/Map.js
@@ -1,23 +1,44 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import tw from "twrnc";
 import MapboxGL from "@react-native-mapbox-gl/maps";
-import { PermissionsAndroid } from 'react-native';
+import { PermissionsAndroid, StyleSheet, Text, View } from "react-native";
 
 MapboxGL.setAccessToken(
     "pk.eyJ1Ijoid2lsbGlhbXdhbmcwNjAyIiwiYSI6ImNrd3Jtc2wwODB3MDgyb3A0enp1ZWcycXYifQ.gLTdJRa1iYiQWVurp0WBQQ"
 );
 
-const Map = () => {
+async function requestLocationPermission() {
+    try {
+        const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+                title: "Location Permission",
+                message: "This app needs access your location to work.",
+                buttonNeutral: "Ask Me Later",
+                buttonNegative: "Cancel",
+                buttonPositive: "OK",
+            }
+        );
+        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log("You can use the location");
+        } else {
+            console.log("Location permission denied");
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+function Map() {
+    requestLocationPermission();
     return (
         <View style={styles.container}>
             <MapboxGL.MapView style={styles.map}>
-                <MapboxGL.UserLocation showsUserHeadingIndicator={true}/>
+                <MapboxGL.UserLocation showsUserHeadingIndicator={true} />
             </MapboxGL.MapView>
         </View>
     );
-};
-
+}
 
 const styles = StyleSheet.create({
     container: {
