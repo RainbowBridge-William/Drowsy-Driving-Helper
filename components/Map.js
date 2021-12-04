8e41ea234a160ab7ab040255e23535d73e0157bb
William Wang <swang45@uw.edu>
2021-12-04 12:11:24 -0800

    Added User Location Marker

--- components/Map.js
+++ components/Map.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import tw from "twrnc";
 import MapboxGL from "@react-native-mapbox-gl/maps";
+import { PermissionsAndroid } from 'react-native';
 
 MapboxGL.setAccessToken(
     "pk.eyJ1Ijoid2lsbGlhbXdhbmcwNjAyIiwiYSI6ImNrd3Jtc2wwODB3MDgyb3A0enp1ZWcycXYifQ.gLTdJRa1iYiQWVurp0WBQQ"
@@ -10,12 +11,13 @@ MapboxGL.setAccessToken(
 const Map = () => {
     return (
         <View style={styles.container}>
-            <MapboxGL.MapView style={styles.map} />
+            <MapboxGL.MapView style={styles.map}>
+                <MapboxGL.UserLocation showsUserHeadingIndicator={true}/>
+            </MapboxGL.MapView>
         </View>
     );
 };
 
-export default Map;
 
 const styles = StyleSheet.create({
     container: {
@@ -26,3 +28,4 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 });
+export default Map;
