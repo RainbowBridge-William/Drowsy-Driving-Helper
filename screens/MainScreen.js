2343fe2829c2821948d5fa9ca62ff6448b885687
William Wang <swang45@uw.edu>
2021-12-15 14:57:22 -0800

    Prepared timer

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -11,30 +11,30 @@ import { selectDestination, selectOrigin, setDestination, setOrigin } from "../s
 const MainScreen = () => {
     const [rest, setRest] = useState(false);
     const dispatch = useDispatch();
+
+    function handleTimesUP() {
+        setRest(true);
+        console.log("data fetched");
+        Geolocation.getCurrentPosition(
+            (position) => {
+                let userLng = position.coords.longitude;
+                let userLat = position.coords.latitude;
+                dispatch(setOrigin(position.coords));
+                getNearestRestArea(userLng, userLat).then((res) => {
+                    dispatch(setDestination(res));
+                });
+            },
+            (error) => {
+                console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+    }
+
     return (
         <View style={tw `bg-white h-full w-full`}>
             <Timer></Timer>
-            <Button title="track" onPress={ () => {
-                setRest(true);
-                console.log("data fetched")
-                Geolocation.getCurrentPosition(
-                    async (position) => {
-                        let userLng = position.coords.longitude;
-                        let userLat = position.coords.latitude;
-                        dispatch(setOrigin(position.coords));
-                        await getNearestRestArea(userLng, userLat)
-                        .then((res) => {
-                            dispatch(setDestination(res))
-                        })
-                    },
-                    (error) => {
-                        // See error code charts below.
-                        console.log(error.code, error.message);
-                    },
-                    { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-                );
-            }
-            } />
+            <Button title="track" onPress={ handleTimesUP} />
             <Map rest={rest}></Map>
             {/* <Button style={tw `h-4 w-4`} title="nav test" onPress={getLocation}></Button> */}
         </View>
