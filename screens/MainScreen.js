1cef03c20f0323ec211f6f273d578fadec5b2917
William Wang <swang45@uw.edu>
2021-12-15 23:22:29 -0800

    Cleaned Import

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -3,10 +3,6 @@ import { StyleSheet, Text, View, SafeAreaView, Button } from 'react-native';
 import tw from "twrnc";
 import Timer from '../components/Timer';
 import Map, { getLocation } from '../components/Map';
-import Geolocation from "react-native-geolocation-service";
-import getNearestRestArea from "../components/getNearestRestArea";
-import { useDispatch, useSelector } from "react-redux";
-import { selectDestination, selectOrigin, setDestination, setOrigin } from "../slices/navSlice";
 
 const MainScreen = () => {
     const [rest, setRest] = useState(false); // you should rest == false
